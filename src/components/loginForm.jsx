3ff081a4f0f6549a8474493b8d321cdcfbb60188
Modekai <muzyar.rad@gmail.com>
2019-10-03 18:04:25 -0700

    using controlled State to update the username field of login form.

---
+++ src/components/loginForm.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              value={this.state.account.username}
+              onChange={this.handleChange}
+              autoFocus
+              id="username"
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+        </form>
+        <button className="btn btn-primary">Login</button>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
