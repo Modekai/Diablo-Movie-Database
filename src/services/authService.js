4e44020b7b435cf8eba21d74ec375f69be860859
Modekai <muzyar.rad@gmail.com>
2019-10-16 17:35:53 -0700

    Refactored and placed all the authentications using token jwt into one single component called authService.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,35 @@
 import http from "./httpService";
+import jwtDecode from "jwt-decode";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
 
 export function login(email, password) {
-  return http.post(apiEndpoint, { email, password });
+  const { data: jwt } = http.post(apiEndpoint, { email, password });
+  localStorage.setItem(tokenKey, jwt);
 }
+
+export function loginWithJwt(jwt) {
+  localStorage.setItem(tokenKey, jwt);
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+
+export function logout() {
+  localStorage.removeItem(tokenKey);
+}
+
+export default {
+  login,
+  logout,
+  getCurrentUser,
+  loginWithJwt
+};
